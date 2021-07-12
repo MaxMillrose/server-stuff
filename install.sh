6fbc9d389d2b6661710dc2b09cd448109ddd3663
Max Millrose <max.millrose@gmail.com>
2021-07-12 22:58:34 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,15 @@
 
 apt install software-properties-common -y &&
 add-apt-repository ppa:neovim-ppa/unstable && apt update &&
-apt install apt-transport-tor fail2ban fish screen neovim -y &&
+apt install apt-transport-tor tor fail2ban fish screen neovim -y &&
 echo "deb [arch=amd64] tor://sdscoq7snqtznauu.onion/torproject.org focal main" >> /etc/apt/sources.list
 apt update && apt dist-upgrade -y 
+
+useradd -m millrose 
+usermod -aG millrose sudo millrose
+echo -e "\n \t Please enter PW for millrose"
+passwd millrose
+
 
 cp -vr etc/fail2ban/* /etc/fail2ban/
 cp -vr root/* /root/
