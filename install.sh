b116193dd3af5e297f4797af10fc252542120b45
Max Millrose <max.millrose@gmail.com>
2021-07-12 22:42:31 +0200

    Install-script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
-add-apt-repository ppa:neovim-ppa/unstable
-apt install apt-transport-tor fail2ban fish screen 
+apt install software-properties-common -y &&
+add-apt-repository ppa:neovim-ppa/unstable &&
+apt install apt-transport-tor fail2ban fish screen neovim-y &&
 echo "deb [arch=amd64] tor://sdscoq7snqtznauu.onion/torproject.org focal main" >> /etc/apt/sources.list
-apt update && apt dist-upgrade
+apt update && apt dist-upgrade -y 
 
+cp -vr etc/fail2ban/* /etc/fail2ban/
+cp -vr root/* /root/
 
