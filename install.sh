e087cd5dbf6a11f3fb000e94d521337962364667
Max Millrose <max.millrose@gmail.com>
2021-07-12 22:52:39 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt install software-properties-common -y &&
-add-apt-repository ppa:neovim-ppa/unstable &&
+add-apt-repository ppa:neovim-ppa/unstable && apt update &&
 apt install apt-transport-tor fail2ban fish screen neovim -y &&
 echo "deb [arch=amd64] tor://sdscoq7snqtznauu.onion/torproject.org focal main" >> /etc/apt/sources.list
 apt update && apt dist-upgrade -y 
