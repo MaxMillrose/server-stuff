2d4007a025c627416666cc72cc7989723294d610
Max Millrose <max.millrose@gmail.com>
2021-07-12 22:46:56 +0200

    fixed typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt install software-properties-common -y &&
 add-apt-repository ppa:neovim-ppa/unstable &&
-apt install apt-transport-tor fail2ban fish screen neovim-y &&
+apt install apt-transport-tor fail2ban fish screen neovim -y &&
 echo "deb [arch=amd64] tor://sdscoq7snqtznauu.onion/torproject.org focal main" >> /etc/apt/sources.list
 apt update && apt dist-upgrade -y 
 
