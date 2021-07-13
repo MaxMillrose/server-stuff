09e7ad519ad6919e8a4a98f8b99d055bffc4b8d6
Max Millrose <max.millrose@gmail.com>
2021-07-13 08:13:47 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 export DEBIAN_FRONTEND=noninteractive 
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
+useradd -m millrose 
+usermod -aG millrose sudo millrose
+echo -e "\n\n\n Please enter PW for millrose"
+passwd millrose
+
 echo "tzdata tzdata/Areas select Europe" > preseed.txt
 echo "" >> preseed.txt 
 debconf-set-selections preseed.txt 
@@ -14,12 +19,10 @@ apt install apt-transport-tor tor fail2ban fish screen neovim -y &&
 echo "deb [arch=amd64] tor://sdscoq7snqtznauu.onion/torproject.org focal main" >> /etc/apt/sources.list
 apt update && apt dist-upgrade -y 
 
-useradd -m millrose 
-usermod -aG millrose sudo millrose
-echo -e "\n \t Please enter PW for millrose"
-passwd millrose
 
 
 cp -vr etc/fail2ban/* /etc/fail2ban/
 cp -vr root/* /root/
+
+echo "Remember to init a SSH-Session via the user before reloading Fail2Ban"
 
