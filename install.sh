ff4175cd4738fcf3fa634f2580199e6c8a3e4984
Max Millrose <max.millrose@gmail.com>
2021-07-16 22:54:57 +0200

    stuff

--- install.sh
+++ install.sh
@@ -16,8 +16,12 @@ debconf-set-selections preseed.txt
 apt install software-properties-common -y &&
 add-apt-repository ppa:neovim-ppa/unstable -y && apt update &&
 apt install apt-transport-tor tor fail2ban fish screen neovim -y &&
+
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 echo "deb [arch=amd64] tor://sdscoq7snqtznauu.onion/torproject.org focal main" >> /etc/apt/sources.list
 service tor start && echo -e "\nsleeping 10 seconds to give Tor time to bootstrap" && sleep 10 
+#clear 
 apt update && apt dist-upgrade -y 
 
 
