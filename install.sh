59c08a956ae1ba0243fcd16fcc8ea7283d8262b5
Max Millrose <max.millrose@gmail.com>
2021-07-16 22:42:08 +0200

    fix typo

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 export DEBIAN_FRONTEND=noninteractive 
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
-useradd -m millrose 
-usermod -aG millrose sudo millrose
-echo -e "\n\n\n Please enter PW for millrose"
-passwd millrose
+useradd -m user-account 
+usermod -aG sudo user-account
+echo -e "\n\n\n Please enter PW for user-account"
+passwd user-account
 
 echo "tzdata tzdata/Areas select Europe" > preseed.txt
 echo "" >> preseed.txt 
@@ -14,7 +14,7 @@ debconf-set-selections preseed.txt
 
 
 apt install software-properties-common -y &&
-add-apt-repository ppa:neovim-ppa/unstable && apt update &&
+add-apt-repository ppa:neovim-ppa/unstable -y && apt update &&
 apt install apt-transport-tor tor fail2ban fish screen neovim -y &&
 echo "deb [arch=amd64] tor://sdscoq7snqtznauu.onion/torproject.org focal main" >> /etc/apt/sources.list
 apt update && apt dist-upgrade -y 
